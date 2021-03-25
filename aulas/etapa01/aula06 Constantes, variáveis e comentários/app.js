9958cf6e25e4d89b97964c3fa17aa578fa1080cf
Tiago Santos <tisgo.slip@hotmail.com>
2021-03-25 08:25:31 -0300

    etapa01

---
+++ aulas/etapa01/aula06 Constantes, variáveis e comentários/app.js
@@ -0,0 +1,24 @@
+let age = 33
+let currentYear = 2021
+console.log(age, currentYear)
+
+age = 34
+console.log(age)
+
+const points = 100
+// points = 101 comentário de uma linha
+console.log(points)
+
+var score = 75
+console.log(score)
+score = 76
+console.log(score)
+
+/*
+comentário
+multi
+linhas
+*/
+
+let day = 25
+console.log(day)
