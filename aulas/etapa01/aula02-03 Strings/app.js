d7c209666e235e01dca06b6932df00c11f0113bc
Tiago Santos <tisgo.slip@hotmail.com>
2021-04-07 23:09:01 -0300

    wip aula01

---
+++ aulas/etapa01/aula02-03 Strings/app.js
@@ -0,0 +1,37 @@
+// strings
+
+console.log("hello world")
+
+const email = "tisgo.slip@hotmail.com"
+
+console.log(email)
+
+// concatenação de strings
+
+const firstName = "Tiago"
+const lastName = "Santos"
+const fullName = firstName + " " + lastName
+
+console.log(fullName)
+
+// acessando caracteres
+
+console.log(fullName[6]);
+
+// comprimento da string
+
+console.log(fullName.length);
+
+// métodos string
+
+console.log(fullName.toUpperCase());
+
+const result = fullName.toLowerCase()
+
+console.log(result);
+
+console.log(fullName);
+
+const index = email.indexOf('@')
+
+console.log(index);
